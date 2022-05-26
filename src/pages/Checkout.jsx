b55f454c6ec9a9f366863cd93ed8f9317f5c073e
MancorJ <mr.jacksonator@gmail.com>
2022-05-25 20:12:19 -0600

    Updated Checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,14 +1,27 @@
 import classes from "./Checkout.module.css";
 
+const testOrder = [{"order":{"orderId":"test-id","productNames":["baby yoda oven mitt"],"orderDate":1653428474242,"customerId":"dummy-id"}
+,"responseStatus":{"code":200,"message":"[SUCCESS] Good job everything is working great."}}]
 
 
-function Checkout(props) {
-    const orderJsx = props.data.map((order) => (
-      <div key={order.orderId} className={classes.order}>
-        <h3 className={classes.name}>{order.productNames}</h3>
-        <div className={classes.date}>{order.orderDate}</div>
-      </div>
-    ));
+let tableIsRendered = false;
+const getOrder = () => {
+  if (JSON.parse(sessionStorage.getItem("order") != null)) {
+    tableIsRendered = true;
+    console.log("Order was found in session storage");
+  } else {
+    console.log("Order was not found in session storage");
+  }
+};
+
+
+function Checkout() {
+  //var order = JSON.parse(sessionStorage.getItem("order"));
+  var order = testOrder;
+  if (order != null) {
+    const orderProductsArray = order.productNames;
+    //i want to create a string of all the productNames by using .join(" ") but it's giving errors.  //TODO
+  }
     return (
         <div>
           <title>Checkout Successful</title>
@@ -16,13 +29,24 @@ function Checkout(props) {
               <h1>Checkout Complete!</h1>
               <p>Your order is on its way!</p>
               <button className={classes.reviewbutton}
-               onClick={() => props.content("test-id")}>
+               onClick={getOrder}>
                    Click here to view your order</button>
             </div>
-            
             <h2>test</h2>
-            
-            <div className={classes.content}>{orderJsx}</div>
+            {tableIsRendered && <div>
+              <table>
+              <tr>
+                <th>Order Id</th>
+                <th>Order Date</th>
+              </tr>
+              <tbody>
+                <tr>
+                  ${order.orderId}
+                  ${order.orderDate}
+                </tr>
+              </tbody>
+            </table> 
+            </div>}
         </div>
         
       );
